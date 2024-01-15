83e53bb05b490a1603346c40737c94776934c083
mbabaziemelyne <uwimbeme@gmail.com>
2024-01-15 14:51:01 +0200

    fifo

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""fifo caching"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """inherits from base_caching"""
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """assign to the dict"""
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                item_to_be_discarded = self.key_indexes.pop(0)
+                del self.cache_data[item_to_be_discarded]
+                print(f"DISCARD: {item_to_be_discarded}\n")
+
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """returns the value in dict"""
+        if key is None or self.cache_data is None:
+            return None
+        return self.cache_data[key]
