ca4ce995ba470e0092d3ee326cb37b7ade589951
mbabaziemelyne <uwimbeme@gmail.com>
2024-01-15 14:54:03 +0200

    fifo

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -18,12 +18,10 @@ class FIFOCache(BaseCaching):
                 item_to_be_discarded = self.key_indexes.pop(0)
                 del self.cache_data[item_to_be_discarded]
                 print(f"DISCARD: {item_to_be_discarded}\n")
-
-
             self.cache_data[key] = item
 
     def get(self, key):
         """returns the value in dict"""
-        if key is None or self.cache_data is None:
+        if key is None or self.cache_data[key] is None:
             return None
         return self.cache_data[key]
